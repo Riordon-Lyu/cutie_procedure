64d65b87e87567681c5ce8652324b69763c03d8e
Riordon-Lyu <81798966+Riordon-Lyu@users.noreply.github.com>
2021-04-04 18:37:21 +0800

    Update cute_procedure.py

--- cute_procedure.py
+++ cute_procedure.py
@@ -9,19 +9,20 @@ input=st.text_input('请输入进入密码：')
 
 if input=='么么哒':
     st.success('贼聪明！进入下一关')
-
     x=st.slider('请给我打分：')
     if x<90:
         st.warning(random.choice(sign_word_list))
     elif 90<x<100:
-        st.info('嘻嘻')
-        time.sleep(2)
-        st.warning('但还是不对哦！')
+        st.info('嘻嘻,但....')
+        time.sleep(1.5)
+        st.warning('还是不对哦！')
     else:
         st.success('恭喜答对啦！')
         st.balloons()
+        st.markdown('![](https://z3.ax1x.com/2021/04/04/cKBFPg.png)')
 
-
+else:
+    '呆呆小沙雕！哈哈哈~'
 
 
 
