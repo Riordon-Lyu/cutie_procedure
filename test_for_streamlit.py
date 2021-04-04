2a0d06e6a9db8a5d221caef6fee7f47316f1dbf8
Riordon-Lyu <81798966+Riordon-Lyu@users.noreply.github.com>
2021-04-04 18:00:39 +0800

    Update test_for_streamlit.py

--- test_for_streamlit.py
+++ test_for_streamlit.py
@@ -1,16 +1,27 @@
 import pandas as pd 
 import numpy as np 
 import streamlit as st 
+import time,random
 
+sign_word_list=['啊?', '哈?!', '咦?~', '喵喵喵！', 'what？', '嘤嘤嘤~','天啦撸！']
 
-input=st.text_input('请输入：')
+input=st.text_input('请输入进入密码：')
 
 if input=='么么哒':
-    '贼聪明'
-    st.balloons()
-elif input=='':
-    st.write('')
-else:
-    '小沙雕，嘻嘻嘻~'
+    st.success('贼聪明！进入下一关')
+
+    x=st.slider('请给我打分：')
+    if x<90:
+        st.warning(random.choice(sign_word_list))
+    elif 90<x<100:
+        st.info('嘻嘻')
+        time.sleep(2)
+        st.warning('但还是不对哦！')
+    else:
+        st.success('恭喜答对啦！')
+        st.balloons()
+
+
+
 
 
