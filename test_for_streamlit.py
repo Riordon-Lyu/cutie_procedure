c6368ca4bfde7801087a78c8174a8b3978e3fb85
Riordon-Lyu <81798966+Riordon-Lyu@users.noreply.github.com>
2021-04-04 17:21:19 +0800

    Update test_for_streamlit.py

--- test_for_streamlit.py
+++ test_for_streamlit.py
@@ -2,8 +2,14 @@ import pandas as pd
 import numpy as np 
 import streamlit as st 
 
-df=pd.read_excel('港股对标公司.xlsx')
 
-df
+input=st.text_input('请输入：')
+
+if input=='么么哒':
+    '贼聪明'
+elif input=='':
+    st.write('')
+else:
+    '小沙雕，嘻嘻嘻~'
 
 
